8ebafcee84200d5296721bc900303d0215befefb
Inrix <Inrixia@gmail.com>
2022-07-14 22:50:42 +1200

    Exported type InteractionResponse

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 export { createApplicationCommandHandler, Application, Command } from "./handler";
 export * from "./permissions";
-export type { CommandInteractionHandler, ComponentInteractionHandler, InteractionDataType } from "./interaction";
+export type { CommandInteractionHandler, ComponentInteractionHandler, InteractionDataType, InteractionResponse } from "./interaction";
 import type { APIApplicationCommand } from "discord-api-types/payloads";
 
 export type ApplicationCommand = Partial<APIApplicationCommand>;
