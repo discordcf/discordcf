8a7e96ea08cf7a89c838e74353d5d08ce5c45ad2
Inrix <Inrixia@gmail.com>
2022-10-25 21:22:57 +1300

    Update readme

--- examples/hello/src/index.ts
+++ examples/hello/src/index.ts
@@ -2,24 +2,26 @@ import { createApplicationCommandHandler, Permissions, type ApplicationCommandHa
 import { hug } from "./hug";
 
 export type EnvInterface = {
-	// Secrets
-	applicationId: string;
-	botToken: string;
-	publicKey: string;
+  // Secrets
+  applicationId: string;
+  botToken: string;
+  publicKey: string;
 };
 
 let handler: ApplicationCommandHandler;
 export default {
-	fetch: (req: Request, env: EnvInterface, context: ExecutionContext) => {
-		if (handler === undefined) {
-			handler = createApplicationCommandHandler({
-				applicationId: env.applicationId,
-				botToken: env.botToken,
-				publicKey: env.publicKey,
-				commands: [hug],
-				permissions: new Permissions([]),
-			});
-		}
-		return handler(req, env, context);
-	},
+  fetch: (req: Request, env: EnvInterface, context: ExecutionContext) => {
+    if (handler === undefined) {
+      handler = createApplicationCommandHandler({
+        applicationId: env.applicationId,
+        botToken: env.botToken,
+        publicKey: env.publicKey,
+        // Optional guildId for development
+        // guildId: "",
+        commands: [hug],
+        permissions: new Permissions([]),
+      });
+    }
+    return handler(req, env, context);
+  },
 };
